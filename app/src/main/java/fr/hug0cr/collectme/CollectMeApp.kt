c4955f78772c9cd2c32de3da727102d6342410f6
hug0cr <pro.hugocamacho@gmail.com>
2023-02-14 15:18:04 +0100

    [app-state] Mise en place de l'état général de l'application.

--- app/src/main/java/fr/hug0cr/collectme/CollectMeApp.kt
+++ app/src/main/java/fr/hug0cr/collectme/CollectMeApp.kt
@@ -1,18 +1,79 @@
 package fr.hug0cr.collectme
 
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import android.content.res.Resources
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import fr.hug0cr.collectme.common.snackbar.SnackbarManager
+import fr.hug0cr.collectme.screens.splash.SplashScreen
 import fr.hug0cr.collectme.ui.theme.CollectMeTheme
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 @ExperimentalMaterialApi
 fun CollectMeApp() {
     CollectMeTheme {
         Surface(color = MaterialTheme.colors.background) {
-            Text(text = "Hello CollectMeApp")
+            val appState = rememberAppState()
+
+            Scaffold(
+                snackbarHost = {
+                    SnackbarHost(
+                        hostState = it,
+                        modifier = Modifier.padding(8.dp),
+                        snackbar = { snackbarData ->
+                            Snackbar(snackbarData, contentColor = MaterialTheme.colors.onPrimary)
+                        }
+                    )
+                },
+                scaffoldState = appState.scaffoldState
+            ) { innerPaddingModifier ->
+                NavHost(
+                    navController = appState.navController,
+                    startDestination = SPLASH_SCREEN,
+                    modifier = Modifier.padding(innerPaddingModifier)
+                ) {
+                    collectMeGraph(appState)
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun rememberAppState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    navController: NavHostController = rememberNavController(),
+    snackbarManager: SnackbarManager = SnackbarManager,
+    resources: Resources = resources(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+) =
+    remember(scaffoldState, navController, snackbarManager, resources, coroutineScope) {
+        CollectMeAppState(scaffoldState, navController, snackbarManager, resources, coroutineScope)
+    }
+
+@Composable
+@ReadOnlyComposable
+fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
+}
+
+@ExperimentalMaterialApi
+fun NavGraphBuilder.collectMeGraph(appState: CollectMeAppState) {
+    composable(SPLASH_SCREEN) {
+        SplashScreen(openAndPopUp = { route, popUp -> appState.navigateAndPopUp(route, popUp) })
     }
 }
