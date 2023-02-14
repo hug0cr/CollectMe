123510b762ca6143896193ed8ad032758d304339
hug0cr <pro.hugocamacho@gmail.com>
2023-02-14 15:18:04 +0100

    [app-state] Mise en place de l'état général de l'application.

--- app/src/main/java/fr/hug0cr/collectme/CollectMeAppState.kt
+++ app/src/main/java/fr/hug0cr/collectme/CollectMeAppState.kt
@@ -1,4 +1,37 @@
 package fr.hug0cr.collectme
 
-class CollectMeAppState {
+import android.content.res.Resources
+import androidx.compose.material.ScaffoldState
+import androidx.navigation.NavHostController
+import fr.hug0cr.collectme.common.snackbar.SnackbarManager
+import kotlinx.coroutines.CoroutineScope
+
+class CollectMeAppState(
+    val scaffoldState: ScaffoldState,
+    val navController: NavHostController,
+    private val snackbarManager: SnackbarManager,
+    private val resources: Resources,
+    coroutineScope: CoroutineScope,
+) {
+    fun popUp() {
+        navController.popBackStack()
+    }
+
+    fun navigate(route: String) {
+        navController.navigate(route) { launchSingleTop = true }
+    }
+
+    fun navigateAndPopUp(route: String, popUp: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(popUp) { inclusive = true }
+        }
+    }
+
+    fun clearAndNavigate(route: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(0) { inclusive = true }
+        }
+    }
 }
