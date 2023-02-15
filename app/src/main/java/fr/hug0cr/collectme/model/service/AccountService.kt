c864157c32fbca15701ad781c13e07d586449bb8
hug0cr <pro.hugocamacho@gmail.com>
2023-02-15 14:24:02 +0100

    [authentication-service] Création du service d'authentification avec une implémentation pour Firebase.

---
+++ app/src/main/java/fr/hug0cr/collectme/model/service/AccountService.kt
@@ -0,0 +1,18 @@
+package fr.hug0cr.collectme.model.service
+
+import fr.hug0cr.collectme.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface AccountService {
+    val currentUserId: String
+    val hasUser: Boolean
+
+    val currentUser: Flow<User>
+
+    suspend fun authenticate(email: String, password: String)
+    suspend fun sendRecoveryEmail(email: String)
+    suspend fun createAnonymousAccount()
+    suspend fun linkAccount(email: String, password: String)
+    suspend fun deleteAccount()
+    suspend fun signOut()
+}
