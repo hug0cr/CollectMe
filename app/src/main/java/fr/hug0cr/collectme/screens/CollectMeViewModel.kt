9af31cc716b34e1678637059491c71f81595bf1c
hug0cr <pro.hugocamacho@gmail.com>
2023-02-15 09:22:44 +0100

    [misc] Ajout TODO.

--- app/src/main/java/fr/hug0cr/collectme/screens/CollectMeViewModel.kt
+++ app/src/main/java/fr/hug0cr/collectme/screens/CollectMeViewModel.kt
@@ -3,4 +3,5 @@ package fr.hug0cr.collectme.screens
 import androidx.lifecycle.ViewModel
 
 open class CollectMeViewModel : ViewModel() {
+    // TODO : Fonction pour attraper les erreurs et les renvoyer à la snackbar
 }
