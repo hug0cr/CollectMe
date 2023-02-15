d0b3cd75dade3226e42c96c0164cdb21e59ca4bb
hug0cr <pro.hugocamacho@gmail.com>
2023-02-15 09:00:44 +0100

    [app-themes] Changement des couleurs et création d'une ressource pour le mode nuit.

--- app/src/main/java/fr/hug0cr/collectme/theme/Theme.kt
+++ app/src/main/java/fr/hug0cr/collectme/theme/Theme.kt
@@ -7,24 +7,14 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+    primary = BrightOrange,
+    primaryVariant = MediumOrange,
+    secondary = DarkOrange)
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = BrightOrange,
+    primaryVariant = MediumOrange,
+    secondary = DarkOrange
 )
 
 @Composable
